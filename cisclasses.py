118316c293791c083912d147615bae5d220ce5f8
SheriseZ <33915324+SheriseZ@users.noreply.github.com>
2018-03-16 09:58:39 -0700

    Add files via upload

---
+++ cisclasses.py
@@ -0,0 +1,5 @@
+# lab 8
+# Zhubo Deng, Shiyu Zhang
+
+class CISclasseses:
+    def __init__(self, )
---
+++ lab8.py
@@ -0,0 +1,6 @@
+# lab 8
+# Shiyu Zhang, Zhubo Deng
+# Write an application that helps a user look up CIS classes. 
+# The app uses data from this CIS web page, which has been saved into a text file called lab8input.txt. 
+# The app lets the user look up CIS classes by entering a class number, or a CIS topic, or a CIS topic and a quarter.
+
