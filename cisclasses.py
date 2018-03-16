def3809222bd2ec71eea419a0aac999a4ddb8cad
SheriseZ <sherisezhang@gmail.com>
2018-03-16 11:00:00 -0700

    update cisclasses class

--- cisclasses.py
+++ cisclasses.py
@@ -2,4 +2,17 @@
 # Zhubo Deng, Shiyu Zhang
 
 class CISclasseses:
-    def __init__(self, i):
+    def __init__(self):
+        filename1 = "lab8input.txt"
+        filename2 = "lab8_no_blankline.txt"
+        try:
+            with open(filename2,'r') as fin:
+                for line in fin:
+                    line = line.strip()
+                    print(line)
+        except IOError as e1:
+            print(str(e1))
+            return None
+        
+        
+# classes = CISclasseses()
